77db94fa098929ded3a42384eb278253952b3a75
nguyenminhtuan100802 <nguyenminhtuan100802@gmail.com>
2024-12-10 09:11:15 +0700

    Init project

---
+++ src/test/java/com/tuan/exam/ExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuan.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
